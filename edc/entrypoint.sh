8815c47a45fdbac417e13fd38e56b0227450f0dc
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2023-09-20 15:46:24 +0200

    Updated EDC addresses

--- edc/entrypoint.sh
+++ edc/entrypoint.sh
@@ -4,7 +4,8 @@
 cp /configs/$EDC_FS_CONFIG $EDC_FS_CONFIG
 
 # Replace the placeholders in the copied properties file with the values of the environment variables
-sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" $EDC_FS_CONFIG
+sed -i "s/\${EDC_ADDRESS}/$EDC_ADDRESS/g" $EDC_FS_CONFIG
+sed -i "s/\${AWS_ADDRESS}/$AWS_ADDRESS/g" $EDC_FS_CONFIG
 sed -i "s/\${POSTGRES_USER}/$POSTGRES_USER/g" $EDC_FS_CONFIG
 sed -i "s/\${POSTGRES_PASSWORD}/$POSTGRES_PASSWORD/g" $EDC_FS_CONFIG
 
