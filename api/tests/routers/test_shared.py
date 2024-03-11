c3e09a38cbd2f78c89c401592fa37351e0d976d9
sply88 <sebastian.pauly@tu-dortmund.de>
2024-03-11 10:47:19 +0100

    Update test_shared.py
    New tests for list_cases endpoint

--- api/tests/routers/test_shared.py
+++ api/tests/routers/test_shared.py
@@ -93,12 +93,28 @@ def case_id():
 
 
 @pytest.fixture
-def case_data(case_id):
+def customer_id():
+    return "anonymous"
+
+
+@pytest.fixture
+def vin():
+    return "test-vin"
+
+
+@pytest.fixture
+def workshop_id():
+    return "test-workshop-id"
+
+
+@pytest.fixture
+def case_data(case_id, customer_id, vin, workshop_id):
     """Valid minimal case data."""
     return {
         "_id": case_id,
-        "vehicle_vin": "test-vin",
-        "workshop_id": "test-workshop-id",
+        "customer_id": customer_id,
+        "vehicle_vin": vin,
+        "workshop_id": workshop_id,
     }
 
 
@@ -178,6 +194,80 @@ def data_context(motor_db, case_data, timeseries_data, obd_data, symptom_data):
     signal_files.drop_indexes()
     signal_chunks.drop()
     signal_chunks.drop_indexes()
+
+
+@pytest.mark.asyncio
+async def test_list_cases_in_empty_db(
+        authenticated_async_client, initialized_beanie_context
+):
+    async with initialized_beanie_context:
+        response = await authenticated_async_client.get("/cases")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+@pytest.mark.asyncio
+async def test_list_cases(
+        authenticated_async_client, initialized_beanie_context, data_context,
+        case_id
+):
+    async with initialized_beanie_context, data_context:
+        response = await authenticated_async_client.get("/cases")
+    assert response.status_code == 200
+    response_data = response.json()
+    assert len(response_data) == 1, "All cases in data_context expected"
+    assert response_data[0]["_id"] == case_id
+
+
+@pytest.mark.parametrize("query_param", ["customer_id", "vin", "workshop_id"])
+@pytest.mark.asyncio
+async def test_list_cases_with_single_filter(
+        authenticated_async_client, initialized_beanie_context, data_context,
+        case_id, query_param, request
+):
+    """Test filtering by a single query param."""
+    query_param_value = request.getfixturevalue(query_param)
+    query_string = f"?{query_param}={query_param_value}"
+    url = f"/cases{query_string}"
+    async with initialized_beanie_context, data_context:
+        response = await authenticated_async_client.get(url)
+        assert response.status_code == 200
+        response_data = response.json()
+        assert len(response_data) == 1, "All cases in data_context expected"
+        assert response_data[0]["_id"] == case_id
+
+
+@pytest.mark.asyncio
+async def test_list_cases_with_multiple_filters(
+        authenticated_async_client, initialized_beanie_context, data_context,
+        case_id, customer_id, vin, workshop_id
+):
+    """Test filtering by multiple query params."""
+    query_string = f"?customer_id={customer_id}&" \
+                   f"vin={vin}&" \
+                   f"workshop_id={workshop_id}"
+    url = f"/cases{query_string}"
+    async with initialized_beanie_context, data_context:
+        response = await authenticated_async_client.get(url)
+        assert response.status_code == 200
+        response_data = response.json()
+        assert len(response_data) == 1, "All cases in data_context expected"
+        assert response_data[0]["_id"] == case_id
+
+
+@pytest.mark.parametrize("query_param", ["customer_id", "vin", "workshop_id"])
+@pytest.mark.asyncio
+async def test_list_cases_with_unmatched_filters(
+        authenticated_async_client, initialized_beanie_context, data_context,
+        query_param
+):
+    """Test filtering using query params not in test data_context."""
+    query_string = f"?{query_param}=VALUE_NOT_IN_DATA_CONTEXT"
+    url = f"/cases{query_string}"
+    async with initialized_beanie_context, data_context:
+        response = await authenticated_async_client.get(url)
+        assert response.status_code == 200
+        assert response.json() == []
 
 
 def test_get_case_invalid_id(authenticated_client):
