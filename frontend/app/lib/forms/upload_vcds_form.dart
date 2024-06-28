8ff780ac22c3fafbe5e5e07ce8b12ac17d3ab3e2
Marcel Ruland <info@marcelruland.com>
2024-06-28 15:21:54 +0200

    Implement UploadVcdsForm

--- frontend/app/lib/forms/upload_vcds_form.dart
+++ frontend/app/lib/forms/upload_vcds_form.dart
@@ -1,4 +1,11 @@
+import "dart:typed_data";
+
+import "package:aw40_hub_frontend/components/file_upload_form_component.dart";
+import "package:aw40_hub_frontend/forms/base_upload_form.dart";
+import "package:aw40_hub_frontend/providers/diagnosis_provider.dart";
+import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
 
 class UploadVcdsForm extends StatefulWidget {
   const UploadVcdsForm({super.key});
@@ -8,36 +15,36 @@ class UploadVcdsForm extends StatefulWidget {
 }
 
 class _UploadVcdsFormState extends State<UploadVcdsForm> {
-  bool _isChecked = false;
+  Uint8List? _file;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).colorScheme.onTertiaryContainer,
-          width: 2,
-        ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              runtimeType.toString(),
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Checkbox(
-              value: _isChecked,
-              onChanged: (v) {
-                if (v == null) return;
-                setState(() => _isChecked = v);
-              },
-            ),
-          ],
-        ),
-      ),
+    return BaseUploadForm(
+      content:
+          FileUploadFormComponent(onFileDrop: (Uint8List file) => _file = file),
+      onSubmit: _onSubmit,
     );
+  }
+
+  Future<void> _onSubmit() async {
+    final provider = Provider.of<DiagnosisProvider>(context, listen: false);
+    final messengerState = ScaffoldMessenger.of(context);
+    final Uint8List? file = _file;
+    if (file == null) {
+      messengerState.showSnackBar(
+        SnackBar(
+          content: Text(tr("diagnoses.details.uploadDataErrorMessage")),
+        ),
+      );
+      return;
+    }
+    final bool result = await provider.uploadVcdsData(
+      provider.diagnosisCaseId,
+      file,
+    );
+    final String snackBarText = result
+        ? tr("diagnoses.details.uploadDataSuccessMessage")
+        : tr("diagnoses.details.uploadDataErrorMessage");
+    messengerState.showSnackBar(SnackBar(content: Text(snackBarText)));
   }
 }
