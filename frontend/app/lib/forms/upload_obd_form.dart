ae5b3e26f5640b67c1e7de5548951ae02c0ae004
Marcel Ruland <info@marcelruland.com>
2024-06-28 14:16:22 +0200

    Implement UploadObdForm

--- frontend/app/lib/forms/upload_obd_form.dart
+++ frontend/app/lib/forms/upload_obd_form.dart
@@ -1,4 +1,10 @@
+import "package:aw40_hub_frontend/dtos/new_obd_data_dto.dart";
+import "package:aw40_hub_frontend/forms/base_upload_form.dart";
+import "package:aw40_hub_frontend/providers/diagnosis_provider.dart";
+import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
+import "package:logging/logging.dart";
+import "package:provider/provider.dart";
 
 class UploadObdForm extends StatefulWidget {
   const UploadObdForm({super.key});
@@ -8,36 +14,38 @@ class UploadObdForm extends StatefulWidget {
 }
 
 class _UploadObdFormState extends State<UploadObdForm> {
-  bool _isChecked = false;
+  // ignore: unused_field
+  final Logger _logger = Logger("UploadObdForm");
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).colorScheme.onTertiaryContainer,
-          width: 2,
+    return BaseUploadForm(
+      content: TextFormField(
+        controller: _controller,
+        minLines: 5,
+        maxLines: null,
+        keyboardType: TextInputType.multiline,
+        decoration: const InputDecoration(
+          labelText: "Diagnostic Trouble codes",
+          hintText: "Enter one code per line.",
+          border: OutlineInputBorder(),
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              runtimeType.toString(),
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Checkbox(
-              value: _isChecked,
-              onChanged: (v) {
-                if (v == null) return;
-                setState(() => _isChecked = v);
-              },
-            ),
-          ],
-        ),
-      ),
+      onSubmit: _onSubmit,
     );
+  }
+
+  Future<void> _onSubmit() async {
+    final provider = Provider.of<DiagnosisProvider>(context, listen: false);
+    final messengerState = ScaffoldMessenger.of(context);
+    final List<String> codes = _controller.text.split("\n");
+    final dto = NewOBDDataDto(null, codes);
+    final bool result =
+        await provider.uploadObdData(provider.diagnosisCaseId, dto);
+    final String snackBarText = result
+        ? tr("diagnoses.details.uploadDataSuccessMessage")
+        : tr("diagnoses.details.uploadDataErrorMessage");
+    messengerState.showSnackBar(SnackBar(content: Text(snackBarText)));
   }
 }
