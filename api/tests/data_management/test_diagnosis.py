576bfae5cf0ec73316fd7222e9a916d60a02bb7c
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2024-08-27 18:11:16 +0200

    tests: fixed diagnosis test

--- api/tests/data_management/test_diagnosis.py
+++ api/tests/data_management/test_diagnosis.py
@@ -5,13 +5,13 @@ from api.data_management import Case, Diagnosis
 
 class TestDiagnosis:
 
-    @pytest.mark.asyncio
+    @pytest.mark.asyncio(loop_scope="class")
     async def test_find_in_hub_without_data(self, initialized_beanie_context):
         async with initialized_beanie_context:
             diagnoses = await Diagnosis.find_in_hub(workshop_id="any")
             assert diagnoses == [], "Expected empty list."
 
-    @pytest.mark.asyncio
+    @pytest.mark.asyncio(loop_scope="class")
     async def test_find_in_hub(self, initialized_beanie_context):
         async with initialized_beanie_context:
             # Seed db with 2 cases for workshop "1"
