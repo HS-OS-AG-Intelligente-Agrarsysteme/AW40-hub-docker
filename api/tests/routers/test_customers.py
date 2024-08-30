3f14d9187f894a6bd5a93e6b2e634a81d36c1f00
sply88 <sebastian.pauly@tu-dortmund.de>
2024-08-30 14:58:52 +0200

    Update test_customers.py
    New tests for authentication and authorization

--- api/tests/routers/test_customers.py
+++ api/tests/routers/test_customers.py
@@ -18,7 +18,7 @@ def jwt_payload():
     return {
         "iat": datetime.utcnow().timestamp(),
         "exp": (datetime.utcnow() + timedelta(60)).timestamp(),
-        "preferred_username": "some-user-with-shared-access",
+        "preferred_username": "some-user-with-customers-access",
         "realm_access": {"roles": ["customers"]}
     }
 
@@ -236,3 +236,110 @@ async def test_customer_not_found(
     assert response.status_code == 404
     assert response.json()["detail"] == \
            f"No customer with id '{customer_id}' found."
+
+
+@pytest.mark.parametrize(
+    "route", customers.router.routes, ids=lambda r: r.name
+)
+def test_missing_bearer_token(route, unauthenticated_client):
+    """Endpoints should not be accessible without a bearer token."""
+    assert len(route.methods) == 1, "Test assumes one method per route."
+    method = next(iter(route.methods))
+    response = unauthenticated_client.request(method=method, url=route.path)
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Not authenticated"}
+
+
+@pytest.fixture
+def jwt_payload_with_unauthorized_role(jwt_payload):
+    jwt_payload["realm_access"]["roles"] = ["workshop", "not customers"]
+    return jwt_payload
+
+
+@pytest.fixture
+def signed_jwt_with_unauthorized_role(
+        jwt_payload_with_unauthorized_role, rsa_private_key_pem: bytes
+):
+    return jws.sign(
+        jwt_payload_with_unauthorized_role,
+        rsa_private_key_pem, algorithm="RS256"
+    )
+
+
+@pytest.mark.parametrize(
+    "route", customers.router.routes, ids=lambda r: r.name
+)
+def test_unauthorized_user(
+        route, authenticated_client, signed_jwt_with_unauthorized_role
+):
+    """
+    Endpoints should not be accessible, if the user role encoded in the
+    token does not indicate customers access.
+    """
+    assert len(route.methods) == 1, "Test assumes one method per route."
+    method = next(iter(route.methods))
+    authenticated_client.headers.update(
+        {"Authorization": f"Bearer {signed_jwt_with_unauthorized_role}"}
+    )
+    response = authenticated_client.request(method=method, url=route.path)
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Could not validate token."}
+
+
+@pytest.mark.parametrize(
+    "route", customers.router.routes, ids=lambda r: r.name
+)
+def test_invalid_jwt_signature(
+        route, authenticated_client, another_rsa_public_key_pem
+):
+    """
+    Endpoints should not be accessible, if the public key retrieved from
+    keycloak does not match the private key used to sign a JWT.
+    """
+    assert len(route.methods) == 1, "Test assumes one method per route."
+    method = next(iter(route.methods))
+    # The token signature of the authenticated client will not match the public
+    #  key anymore
+    authenticated_client.app.dependency_overrides[
+        Keycloak.get_public_key_for_workshop_realm
+    ] = lambda: another_rsa_public_key_pem.decode()
+    response = authenticated_client.request(method=method, url=route.path)
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Could not validate token."}
+
+
+@pytest.fixture
+def expired_jwt_payload():
+    return {
+        "iat": (datetime.utcnow() - timedelta(60)).timestamp(),
+        "exp": (datetime.utcnow() - timedelta(1)).timestamp(),
+        "preferred_username": "user",
+        "realm_access": {"roles": ["customers"]}
+    }
+
+
+@pytest.fixture
+def expired_jwt(expired_jwt_payload, rsa_private_key_pem: bytes):
+    """Create an expired JWT signed with private RSA key."""
+    return jws.sign(
+        expired_jwt_payload, rsa_private_key_pem, algorithm="RS256"
+    )
+
+
+@pytest.mark.parametrize(
+    "route", customers.router.routes, ids=lambda r: r.name
+)
+def test_expired_jwt(route, authenticated_client, expired_jwt):
+    """
+    Endpoints should not be accessible, if the bearer token is expired.
+    """
+    assert len(route.methods) == 1, "Test assumes one method per route."
+    method = next(iter(route.methods))
+    # The token offered by the authenticated client is expired
+    response = authenticated_client.request(
+        method=method,
+        url=route.path,
+        headers={"Authorization": f"Bearer {expired_jwt}"}
+    )
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Could not validate token."}
