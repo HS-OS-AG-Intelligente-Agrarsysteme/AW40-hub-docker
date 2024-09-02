93a3efb4a2ec072c10a38462a83031dc412ffc85
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2024-09-02 09:25:02 +0200

    tests: Added new asyncio decorator to key auth test.

--- api/tests/security/test_api_key_auth.py
+++ api/tests/security/test_api_key_auth.py
@@ -1,22 +1,25 @@
 import pytest
+import pytest_asyncio
 from api.security.api_key_auth import APIKeyAuth
 from fastapi import HTTPException
 
 
 class TestAPIKeyAuth:
-
-    def test_no_valid_key_specified(self):
+    @pytest.mark.asyncio(loop_scope="function")
+    async def test_no_valid_key_specified(self):
         key_auth = APIKeyAuth()
         with pytest.raises(AttributeError):
             key_auth("any key")
 
-    def test_successful_validation(self):
+    @pytest.mark.asyncio(loop_scope="function")
+    async def test_successful_validation(self):
         valid_key = "42!"
         key_auth = APIKeyAuth()
         key_auth.valid_key = valid_key
         assert key_auth(valid_key)
 
-    def test_failed_validation(self):
+    @pytest.mark.asyncio(loop_scope="function")
+    async def test_failed_validation(self):
         valid_key = "42!"
         invalid_key = valid_key[1:]
         key_auth = APIKeyAuth()
