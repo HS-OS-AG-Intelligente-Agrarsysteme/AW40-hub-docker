e48198a3fab283320a4c8156ffdee57ba7ef2c8a
sply88 <sebastian.pauly@tu-dortmund.de>
2024-09-02 12:38:51 +0200

    Update test_customers.py
    Test pagination and link headers.

--- api/tests/routers/test_customers.py
+++ api/tests/routers/test_customers.py
@@ -119,6 +119,33 @@ def data_context(
     return DataContext()
 
 
+def parse_and_validate_link_header(header_value):
+    """
+    Parse and validate RFC5988 link header. If validation is successful, a
+    dict mapping the link relations (first, last, prev and next) to the link
+    urls is returned.
+    """
+    # Different links are separated by ", "
+    links = header_value.split(", ")
+    parsed_links = {}
+    for link in links:
+        # URL and relation in each link are separated by "; "
+        (url, rel) = tuple(link.split("; "))
+        # Validate and strip link url
+        assert url[0] == "<" and url[-1] == ">"
+        url = url.strip("<").strip(">")
+        # Validate and strip link relation
+        assert rel[:4] == "rel="
+        rel = rel.strip("rel=")
+        assert rel in [
+            '"first"', '"last"', '"prev"', '"next"'
+        ]
+        rel = rel.strip('"')
+        # Store the mapping
+        parsed_links[rel] = url
+    return parsed_links
+
+
 @pytest.mark.asyncio
 async def test_list_customers_in_empty_db(
         authenticated_async_client, initialized_beanie_context
@@ -127,6 +154,8 @@ async def test_list_customers_in_empty_db(
         response = await authenticated_async_client.get("/")
     assert response.status_code == 200
     assert response.json() == []
+    assert response.headers["link"] == "", \
+        "Link header should be empty if there is no data to retrieve."
 
 
 @pytest.mark.asyncio
@@ -137,9 +166,120 @@ async def test_list_customers(
         n_customers_in_data_context
 ):
     async with initialized_beanie_context, data_context:
+        # Request without any additional params
         response = await authenticated_async_client.get("/")
+    # Validate response status code and data
     assert response.status_code == 200
     assert len(response.json()) == n_customers_in_data_context
+    # Validate linke header
+    links = parse_and_validate_link_header(response.headers["link"])
+    assert links["first"].split("?")[-1] == "page=0&page_size=30", \
+        "Link header should have explicit pagination query."
+    assert links["first"] == links["last"], \
+        "Links for first and last page should be the same as there is only " \
+        "one page in test data_context."
+    assert "prev" not in links and "next" not in links, \
+        "No prev and next links should be included if there are no pages " \
+        "before or after the requested page."
+
+
+@pytest.mark.parametrize(
+    "page,page_size,expected_names",
+    # expected_names list has test data first and last names ordered by
+    # (last, first)
+    [
+        (0, 1, [("A", "A")]),
+        (1, 1, [("B", "A")]),
+        (2, 1, [("A", "B")]),
+        (0, 2, [("A", "A"), ("B", "A")]),
+        (1, 2, [("A", "B")]),
+        (0, 3, [("A", "A"), ("B", "A"), ("A", "B")]),
+        (1, 3, [])
+    ]
+)
+@pytest.mark.asyncio
+async def test_list_customers_pagination(
+        page,
+        page_size,
+        expected_names,
+        authenticated_async_client,
+        initialized_beanie_context
+):
+    async with initialized_beanie_context:
+        # Insert some customers in non-alphabetic order
+        await Customer(first_name="A", last_name="B").create()
+        await Customer(first_name="B", last_name="A").create()
+        await Customer(first_name="A", last_name="A").create()
+        response = await authenticated_async_client.get(
+            f"/?page_size={page_size}&page={page}"
+        )
+        assert response.status_code == 200
+        response_data = response.json()
+        assert [
+                   (_["first_name"], _["last_name"]) for _ in response_data
+               ] == expected_names
+
+
+@pytest.mark.parametrize("page_size", [1, 30])
+@pytest.mark.asyncio
+async def test_list_customers_pagination_valid_page_size_limits(
+        page_size,
+        authenticated_async_client,
+        initialized_beanie_context,
+        data_context,
+        n_customers_in_data_context
+):
+    async with initialized_beanie_context, data_context:
+        response = await authenticated_async_client.get(
+            f"/?page_size={page_size}&page=0"
+        )
+    assert response.status_code == 200
+    assert len(response.json()) == min(page_size, n_customers_in_data_context)
+
+
+@pytest.mark.parametrize("page_size", [-1, 0, 31])
+@pytest.mark.asyncio
+async def test_list_customers_pagination_invalid_page_size_limits(
+        page_size,
+        authenticated_async_client,
+        initialized_beanie_context
+):
+    async with initialized_beanie_context:
+        response = await authenticated_async_client.get(
+            f"/?page_size={page_size}&page=0"
+        )
+    assert response.status_code == 422, \
+        "Expected response to indicate unprocessable content."
+
+
+@pytest.mark.parametrize("page_size", [1, 2, 3, 4])
+@pytest.mark.asyncio
+async def test_list_customers_pagination_links(
+        page_size,
+        authenticated_async_client,
+        initialized_beanie_context
+):
+    async with initialized_beanie_context:
+        # Insert some customers in non-alphabetic order. Note that c_i
+        # is the i-th customer in alphabetic (last_name, first_name) order.
+        c_3 = await Customer(first_name="A", last_name="B").create()
+        c_2 = await Customer(first_name="B", last_name="A").create()
+        c_1 = await Customer(first_name="A", last_name="A").create()
+        # Test retrieval of all docs using the link header for navigation
+        retrieved_docs = []
+        next_page = f"/?page_size={page_size}&page=0"
+        while next_page:
+            response = await authenticated_async_client.get(next_page)
+            assert response.status_code == 200
+            retrieved_docs.extend(response.json())
+            links = parse_and_validate_link_header(response.headers["link"])
+            next_page = links.get("next", None)
+        assert [
+                   _["_id"] for _ in retrieved_docs
+               ] == [
+                   str(_.id) for _ in [c_1, c_2, c_3]
+               ], \
+            "Whole list of resources should be retrieved in alphabetic order."
 
 
 @pytest.mark.asyncio
