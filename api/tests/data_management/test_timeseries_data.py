77909bb86ac335c9c11265831b6270b657792cde
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2024-09-04 10:40:17 +0200

    Revert "tests: (Hack) Added hack to timeseries_data test in data_management"
    This reverts commit b34604a6916f9afa54a29c573755ce5438a1d879.

--- api/tests/data_management/test_timeseries_data.py
+++ api/tests/data_management/test_timeseries_data.py
@@ -7,24 +7,16 @@ from api.data_management.timeseries_data import (
 )
 from beanie import PydanticObjectId
 from pydantic import ValidationError
-from motor import motor_asyncio
 
 
 class TestGridFSSignalStore:
 
     @pytest.mark.asyncio(loop_scope="class")
-    async def test_create_and_get(self, motor_db):
-        # Hack: Cannot use conftest signal_bucket here or test will fail
-        signal_bucket = motor_asyncio.AsyncIOMotorGridFSBucket(
-            motor_db, bucket_name="signals-pytest"
-        )
-
+    async def test_create_and_get(self, signal_bucket):
         signal_store = GridFSSignalStore(signal_bucket)
         signal = [-1., 0., 1]
         signal_id = await signal_store.create(signal)
         retrieved_signal = await signal_store.get(signal_id)
-        motor_db.drop_collection("signals-pytest.files")
-        motor_db.drop_collection("signals-pytest.chunks")
         assert signal == retrieved_signal
 
     @pytest.mark.asyncio(loop_scope="class")
