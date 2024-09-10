4849810900e15b7106c62aa23c40123f5304e153
sply88 <sebastian.pauly@tu-dortmund.de>
2024-09-10 14:38:35 +0200

    Update test_pagination.py
    New test for last_page_index with valid pagination params.

--- api/tests/routers/utils/test_pagination.py
+++ api/tests/routers/utils/test_pagination.py
@@ -3,6 +3,22 @@ import pytest
 from api.routers.utils.pagination import last_page_index, link_header
 
 
+@pytest.mark.parametrize(
+    "page_size,document_count,expected_index",
+    [
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 10, 9),
+        (2, 10, 4),
+        (2, 11, 5)
+    ]
+)
+def test_last_page_index(page_size, document_count, expected_index):
+    assert last_page_index(
+        page_size=page_size, document_count=document_count
+    ) == expected_index
+
+
 @pytest.mark.parametrize("page_size", [1.0, "1"], ids=["float", "str"])
 def test_last_page_index_invalid_page_size_type(page_size):
     with pytest.raises(TypeError):
