5fd8bbaac73c1fd2baf0901cf3cd50f9842b76c1
sply88 <sebastian.pauly@tu-dortmund.de>
2024-10-08 16:03:18 +0200

    Update nautilus.py
    Send actual http request to connector to trigger publication.

--- api/api/dataspace_management/nautilus.py
+++ api/api/dataspace_management/nautilus.py
@@ -1,6 +1,7 @@
 import secrets
 from typing import Optional
-from uuid import uuid4
+
+import httpx
 
 from ..data_management import Asset, Publication
 
@@ -10,17 +11,36 @@ class Nautilus:
     _publication_url: Optional[str] = None
     _market: str = "aw40-market"  # TODO: How is the market determined?
 
+    def __init__(self):
+        if not self._publication_url:
+            raise AttributeError("No Nautilus connection configured.")
+
+    @classmethod
+    def configure(cls, publication_url: str):
+        """Configure nautilus connection details."""
+        cls._publication_url = publication_url
+
     def publish_access_dataset(
             self, asset_url: str, asset: Asset
     ) -> Publication:
-        # Dummy data
-        publication = Publication(
-            did=str(uuid4()),
+        # Generate a new asset key
+        asset_key = secrets.token_urlsafe(32)
+        # Trigger the publication
+        response = httpx.post(
+            self._publication_url,
+            json={
+                "type": "url",
+                "method": "GET",
+                "url": asset_url,
+                "headers": {"asset_key": asset_key}
+            }
+        )
+        response.raise_for_status()  # TODO: Handle failures
+        did = ""  # TODO: Needs to be extracted from response
+
+        return Publication(
+            did=did,
             market=self._market,
-            asset_key=secrets.token_urlsafe(32),
+            asset_key=asset_key,
             asset_url=asset_url
         )
-
-        # TODO: Http request to nautilus connector at self._publication_url
-        # https://github.com/deltaDAO/nautilus-examples/blob/main/publish.ts#L29
-        return publication
