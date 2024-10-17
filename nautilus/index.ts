c860172fe302f5c81a744f4112a2a8e9ed4e5565
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2024-10-17 15:23:35 +0200

    Added swagger ui to nautilus for testing

--- nautilus/index.ts
+++ nautilus/index.ts
@@ -1,9 +1,14 @@
 import express, { Express } from 'express'
 import { healthrouter, nautilusrouter } from './routers'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './openapi.json'
 
 const app: Express = express()
 app.use('/nautilus', nautilusrouter)
 app.use('/health', healthrouter)
+if (process.env.USE_SWAGGER === 'true') {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+}
 
 const port = 3000
 
