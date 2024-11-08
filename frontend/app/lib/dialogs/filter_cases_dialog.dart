e1b3106a9d89134e935cf6c92fca4035adafedde
mgr <mgr@lmis.de>
2024-11-08 12:11:49 +0100

    wip:  added fields to filter dialog

--- frontend/app/lib/dialogs/filter_cases_dialog.dart
+++ frontend/app/lib/dialogs/filter_cases_dialog.dart
@@ -1,4 +1,5 @@
 import "package:aw40_hub_frontend/providers/case_provider.dart";
+import "package:aw40_hub_frontend/text_input_formatters/upper_case_text_input_formatter.dart";
 import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
@@ -37,24 +38,105 @@ class FilterCasesDialogContent extends StatefulWidget {
 }
 
 class _FilterCasesDialogContentState extends State<FilterCasesDialogContent> {
+  final TextEditingController _errorCodeController = TextEditingController();
+  final TextEditingController _vinController = TextEditingController();
+  // TODO Dropdown
+  final TextEditingController _oscillogramController = TextEditingController();
+
+  final List<String> mockDataEntries = ["Test1", "Test2", "Test3"];
+
   bool _switchState = true;
   @override
   Widget build(BuildContext context) {
     _switchState = Provider.of<CaseProvider>(context).showSharedCases;
-    return Row(
-      children: [
-        Text(tr("cases.filterDialog.toggleShared")),
-        Switch(
-          value: _switchState,
-          onChanged: (v) async {
-            setState(() {
-              _switchState = v;
-            });
-            await Provider.of<CaseProvider>(context, listen: false)
-                .toggleShowSharedCases();
-          },
-        ),
-      ],
+    return SizedBox(
+      height: 250,
+      width: 350,
+      child: Column(
+        children: [
+          SizedBox(
+            width: 300,
+            height: 66,
+            child: TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              inputFormatters: [UpperCaseTextInputFormatter()],
+              decoration: const InputDecoration(
+                labelText: "Error code",
+                border: OutlineInputBorder(),
+                errorStyle: TextStyle(height: 0.1),
+              ),
+              controller: _errorCodeController,
+            ),
+          ),
+          const SizedBox(width: 16),
+          SizedBox(
+            width: 300,
+            height: 66,
+            child: TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              inputFormatters: [UpperCaseTextInputFormatter()],
+              decoration: InputDecoration(
+                labelText: "first 6 numbers of vehicle vin",
+                border: const OutlineInputBorder(),
+                errorStyle: const TextStyle(height: 0.1),
+              ),
+              controller: _vinController,
+              validator: (String? value) {
+                if ((value?.length ?? 0) > 6) {
+                  return tr("cases.addCaseDialog.vinLengthInvalid");
+                }
+                if (value != null && value.contains(RegExp("[IOQ]"))) {
+                  return tr(
+                    "cases.addCaseDialog.vinCharactersInvalid",
+                  );
+                }
+
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(width: 16),
+          Tooltip(
+            // TODO adjust (and translate) or remove
+            message: "Please select",
+            child: DropdownMenu<String>(
+              controller: _oscillogramController,
+              label: Text(tr("general.component")),
+              hintText: tr("forms.optional"),
+              enableFilter: true,
+              width: 300,
+              //onSelected: (value) async => _onCustomerSelection(context, value),
+              menuStyle: const MenuStyle(alignment: Alignment.bottomLeft),
+              dropdownMenuEntries:
+                  mockDataEntries.map<DropdownMenuEntry<String>>(
+                (String mockDataEntry) {
+                  return DropdownMenuEntry<String>(
+                    value: mockDataEntry,
+                    label: mockDataEntry,
+                  );
+                },
+              ).toList(),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Text(tr("cases.filterDialog.toggleShared")),
+              const SizedBox(width: 16),
+              Switch(
+                value: _switchState,
+                onChanged: (v) async {
+                  setState(() {
+                    _switchState = v;
+                  });
+                  await Provider.of<CaseProvider>(context, listen: false)
+                      .toggleShowSharedCases();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
