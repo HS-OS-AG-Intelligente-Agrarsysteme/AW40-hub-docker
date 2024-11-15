a8c45ee66b46f6ea4c4cb72294ac77faeb396c4e
Iscia Berger <iscianorina@gmail.com>
2024-11-15 10:47:20 +0100

    Trailing Comma

--- frontend/app/lib/scaffolds/scaffold_wrapper.dart
+++ frontend/app/lib/scaffolds/scaffold_wrapper.dart
@@ -118,21 +118,23 @@ class _ScaffoldWrapperState extends State<ScaffoldWrapper> {
             child: Row(
               children: [
                 Transform.scale(
-                    scale: 0.75,
-                    child: Tooltip(
-                      message: tr("cases.filterDialog.toggleShared"),
-                      child: Switch(
-                        value: _switchState,
-                        onChanged: (v) async {
-                          setState(() {
-                            _switchState = v;
-                          });
-                          await Provider.of<CaseProvider>(context,
-                                  listen: false)
-                              .toggleShowSharedCases();
-                        },
-                      ),
-                    )),
+                  scale: 0.75,
+                  child: Tooltip(
+                    message: tr("cases.filterDialog.toggleShared"),
+                    child: Switch(
+                      value: _switchState,
+                      onChanged: (v) async {
+                        setState(() {
+                          _switchState = v;
+                        });
+                        await Provider.of<CaseProvider>(
+                          context,
+                          listen: false,
+                        ).toggleShowSharedCases();
+                      },
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
